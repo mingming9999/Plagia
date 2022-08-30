4547f3e4240d33d106a077f38de0375f172238be
mingming9999 <112289796+mingming9999@users.noreply.github.com>
2022-08-30 10:31:25 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    f = open("demofile2.txt", "a")
+    f.write("Now the file has more content!")
+    f.close()
+
     return "Hello, World!"
     
 if __name__ == "__main__":
