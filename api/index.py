060ba178fe276a087f935d3b5660b5b160a3cbf0
mingming9999 <112289796+mingming9999@users.noreply.github.com>
2022-08-30 10:52:40 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import request
+from flask import Flask,redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route('/')
+def hello():
+    return redirect("https://Plagiarism-checker.mingming13.repl.co", code=302)
 
-    return "Hello, World!"
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
