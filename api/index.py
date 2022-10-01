7668674aff107db0f971e4d289152218054e7bc2
mingming9999 <112289796+mingming9999@users.noreply.github.com>
2022-10-01 17:32:06 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,18 @@
 import math
 import re
 from collections import Counter
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+
+app.run(host='0.0.0.0', port=81)
+
 
 WORD = re.compile(r"\w+")
 
