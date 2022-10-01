add94e8814c51f6d403243014444334e7ec01dfe
mingming9999 <112289796+mingming9999@users.noreply.github.com>
2022-10-01 17:20:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,35 @@
-from flask import request
-from flask import Flask,redirect
+import math
+import re
+from collections import Counter
 
-app = Flask(__name__)
+WORD = re.compile(r"\w+")
 
-@app.route('/')
-def hello():
-    return redirect("https://Plagiarism-checker.mingming13.repl.co", code=302)
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+
+text1 = "This is a foo bar sentence ."
+text2 = "This sentence is similar to a foo bar sentence ."
+
+vector1 = text_to_vector(text1)
+vector2 = text_to_vector(text2)
+
+cosine = get_cosine(vector1, vector2)
+
+print("Cosine:", cosine)
